6f1ec4eb4444f66d04036edf58cf82dba94bb2db
jiayong <36148963@qq.com>
2018-11-21 09:20:04 +0800

    增加了日志处理

--- encrypt-8081/src/main/java/com/qiaohx/encrypt/controller/TestController.java
+++ encrypt-8081/src/main/java/com/qiaohx/encrypt/controller/TestController.java
@@ -2,14 +2,31 @@ package com.qiaohx.encrypt.controller;
 
 import com.qiaohx.utils.date.DateFormatRules;
 import com.qiaohx.utils.date.DateUtil;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.math.BigDecimal;
+
 @RestController
 public class TestController {
+    private final Logger logger = LoggerFactory.getLogger(this.getClass());
 
     @RequestMapping("/test")
     public String test() {
+        logger.trace("trace log");
+        logger.debug("debug log");
+        logger.info("info log");
+        logger.warn("warn log");
+        logger.error("error log");
+        try {
+            new BigDecimal("a");
+        }catch (Exception e){
+            // 记录异常的方法
+            logger.error("异常", e.getMessage(), e);
+            e.printStackTrace();
+        }
         return DateUtil.dateToStr(DateFormatRules.YYYY_MM_DD_HH_MM_SS);
     }
 }
