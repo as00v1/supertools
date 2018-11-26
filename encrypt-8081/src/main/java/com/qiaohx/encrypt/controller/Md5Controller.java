bceba19c86c9109431fa8eb01031abd83eec918f
jiayong <36148963@qq.com>
2018-11-26 13:46:18 +0800

    配置文件开发、生产分离

--- encrypt-8081/src/main/java/com/qiaohx/encrypt/controller/Md5Controller.java
+++ encrypt-8081/src/main/java/com/qiaohx/encrypt/controller/Md5Controller.java
@@ -2,6 +2,7 @@ package com.qiaohx.encrypt.controller;
 
 import com.qiaohx.encrypt.model.Emd5;
 import com.qiaohx.encrypt.service.md5.IMd5Service;
+import net.sf.json.JSONObject;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -11,6 +12,7 @@ import org.springframework.web.bind.annotation.RestController;
 import javax.annotation.Resource;
 
 @RestController
+@RequestMapping("/md5")
 public class Md5Controller {
 
     private final Logger logger = LoggerFactory.getLogger(this.getClass());
@@ -18,11 +20,13 @@ public class Md5Controller {
     @Resource
     private IMd5Service iMd5Service;
 
-    @RequestMapping("/selectByValue")
+    @RequestMapping("/selectByValue/V1")
     public Emd5 selectByValue(@RequestBody String str){
         Emd5 emd5 = null;
         try {
-            emd5 = iMd5Service.selectByValue(str);
+            JSONObject jsonObject = JSONObject.fromObject(str);
+            String value = jsonObject.containsKey("value") ? jsonObject.getString("value") : "";
+            emd5 = iMd5Service.selectByValue(value);
         }catch (Exception e){
             logger.error("MD5异常", e.getMessage(), e);
             e.printStackTrace();
