1aff065d7fe0f9344b5d4a465ba1cf4fbf90b3e4
jiayong <36148963@qq.com>
2018-11-28 13:57:32 +0800

    定义spring切面记录请求日志

---
+++ utils/src/main/java/com/qiaohx/utils/constant/ConstantCode.java
@@ -0,0 +1,18 @@
+package com.qiaohx.utils.constant;
+
+/**
+ * 响应编码常量
+ * @author jiayong
+ * @since 2018年11月28日13:09:36
+ */
+public class ConstantCode {
+
+    /** 请求成功 */
+    public static final String CODE_200 = "200";
+    public static final String MESSAGE_200 = "请求成功";
+
+    /** 系统出错 */
+    public static final String CODE_500 = "500";
+    public static final String MESSAGE_500 = "SYSTEM ERROR";
+
+}
