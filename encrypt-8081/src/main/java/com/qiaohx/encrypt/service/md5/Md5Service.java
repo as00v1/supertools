3090731cbcc610d6b788ce963f6478efb0395313
jiayong <36148963@qq.com>
2018-12-11 13:24:27 +0800

    优化MD5流程，增加异步处理机制，提高响应效率

--- encrypt-8081/src/main/java/com/qiaohx/encrypt/service/md5/Md5Service.java
+++ encrypt-8081/src/main/java/com/qiaohx/encrypt/service/md5/Md5Service.java
@@ -12,11 +12,24 @@ import javax.annotation.Resource;
 import java.math.BigInteger;
 import java.security.MessageDigest;
 import java.util.Date;
+import java.util.concurrent.ArrayBlockingQueue;
+import java.util.concurrent.LinkedBlockingDeque;
+import java.util.concurrent.ThreadPoolExecutor;
+import java.util.concurrent.TimeUnit;
 
 @Service
 public class Md5Service implements IMd5Service{
 
     private Logger logger = LoggerFactory.getLogger(this.getClass());
+
+    private ThreadPoolExecutor threadPoolExecutor;
+
+    public Md5Service() {
+        logger.debug("Md5Service init");
+        this.threadPoolExecutor = new ThreadPoolExecutor(5, 10, 200, TimeUnit.MILLISECONDS,
+                new LinkedBlockingDeque<Runnable>(5));
+    }
+
     @Resource
     private Emd5Mapper emd5Mapper;
 
@@ -34,20 +47,43 @@ public class Md5Service implements IMd5Service{
             md5 = "0" + md5;
         }
         logger.info("MD5：" + md5);
-        Emd5 emd5 = emd5Mapper.selectByPrimaryKey(md5);
-        if (emd5 == null){
-            logger.info("未查询到数据库中存在，新插入一条");
-            emd5 = new Emd5();
-            emd5.setMd5Key(md5);
-            emd5.setMd5Value(value);
-            emd5.setCreateDate(new Date());
-            emd5Mapper.insert(emd5);
-        }else{
-            logger.info("查询到数据库中存在");
-        }
-        resultJson.put("md5Key", emd5.getMd5Key());
-        resultJson.put("md5Value", emd5.getMd5Value());
+
+        // 放入线程池
+        threadPoolExecutor.execute(new InsertMD5(md5, value));
+
+        resultJson.put("md5Key", md5);
+        resultJson.put("md5Value", value);
         resultJson.put(ConstantCode.ERROR_CODE, ConstantCode.ERROR_CODE_0);
         return resultJson;
+    }
+
+    class InsertMD5 implements Runnable{
+
+        private String md5;
+        private String value;
+
+        public InsertMD5(String md5, String value) {
+            this.md5 = md5;
+            this.value = value;
+        }
+
+        @Override
+        public void run() {
+            logger.info("异步线程新增md5开始");
+            Emd5 emd5 = emd5Mapper.selectByPrimaryKey(md5);
+            if (emd5 == null){
+                logger.info("未查询到数据库中存在，新插入一条");
+                emd5 = new Emd5();
+                emd5.setMd5Key(md5);
+                emd5.setMd5Value(value);
+                final Emd5 emd = new Emd5();
+                emd.setMd5Key(md5);
+                emd.setMd5Value(value);
+                emd.setCreateDate(new Date());
+                emd5Mapper.insert(emd);
+            }else{
+                logger.info("查询到数据库中存在");
+            }
+        }
     }
 }
