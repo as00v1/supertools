321ee0f50d291ffc60d430108da7ccfea9aeb112
jiayong <36148963@qq.com>
2019-03-05 18:16:25 +0800

    测试方法

---
+++ encrypt/src/main/java/com/qiaohx/encrypt/common/des/DESUtil.java
@@ -0,0 +1,75 @@
+package com.qiaohx.encrypt.common.des;
+
+import java.security.SecureRandom;
+import java.util.Base64;
+
+import javax.crypto.Cipher;
+import javax.crypto.SecretKey;
+import javax.crypto.SecretKeyFactory;
+import javax.crypto.spec.DESKeySpec;
+
+public class DESUtil {
+
+    /**
+     * @param args
+     */
+    public static void main(String[] args) {
+        String content = "DESTest";
+        // 密码长度必须是8的倍数
+        String password = "12345678";
+        System.out.println("密　钥：" + password);
+        System.out.println("加密前：" + content);
+        byte[] result = encrypt(content, password);
+        System.out.println("加密后：" + new String(result));
+        String decryResult = decrypt(result, password);
+        System.out.println("解密后：" + decryResult);
+    }
+
+    /**
+     * 加密
+     *
+     * @param content 待加密内容
+     * @param key     加密的密钥
+     * @return
+     */
+    public static byte[] encrypt(String content, String key) {
+        try {
+            SecureRandom random = new SecureRandom();
+            DESKeySpec desKey = new DESKeySpec(key.getBytes());
+            SecretKeyFactory keyFactory = SecretKeyFactory.getInstance("DES");
+            SecretKey securekey = keyFactory.generateSecret(desKey);
+            Cipher cipher = Cipher.getInstance("DES");
+            cipher.init(Cipher.ENCRYPT_MODE, securekey, random);
+            byte[] result = cipher.doFinal(content.getBytes());
+            return Base64.getEncoder().encode(result);
+        } catch (Throwable e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+
+    /**
+     * 解密
+     *
+     * @param content 待解密内容
+     * @param key     解密的密钥
+     * @return
+     */
+    public static String decrypt(byte[] content, String key) {
+        try {
+            SecureRandom random = new SecureRandom();
+            DESKeySpec desKey = new DESKeySpec(key.getBytes());
+            SecretKeyFactory keyFactory = SecretKeyFactory.getInstance("DES");
+            SecretKey securekey = keyFactory.generateSecret(desKey);
+            Cipher cipher = Cipher.getInstance("DES");
+            cipher.init(Cipher.DECRYPT_MODE, securekey, random);
+            byte[] result = cipher.doFinal(content);
+            return new String(result);
+        } catch (Throwable e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+
+
+}
